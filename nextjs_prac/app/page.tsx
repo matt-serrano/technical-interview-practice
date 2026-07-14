a802e625d3d12468268a6544afd93097f8c2c9b3
matt-serrano <180496361+matt-serrano@users.noreply.github.com>
2026-07-14 13:15:18 -0400

    page.tsx completed

---
+++ nextjs_prac/app/page.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+    return (
+        <section> 
+            <h1>Welcome to my website.</h1>
+            <p>This is the homepage.</p>
+            <button>Contact ss</button>
+        </section>
+    );
+}
+
+
+// app/
+// ├── layout.tsx
+// ├── page.tsx
+// └── about/
+//     └── page.tsx -> To create another page
