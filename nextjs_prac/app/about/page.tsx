e66109bb2d4588e3a701cad072af6db9056748b8
Serrano <matthew.serrano@ontario.ca>
2026-07-21 11:36:25 -0400

    adding unadded folder and files

---
+++ nextjs_prac/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <section>
+            <h1>About us</h1>
+            <p>Learn more about our business.</p>
+        </section>
+    )
+}
